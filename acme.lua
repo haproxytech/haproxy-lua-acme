1b055ed14871e9c2ac06d0c47104fc618f6ab4d4
Adis Nezirović <anezirovic@haproxy.com>
2018-05-15 14:29:56 +0200

    Add license notice to the code.

--- acme.lua
+++ acme.lua
@@ -8,7 +8,10 @@
 --
 -- Copyright (c) 2017-2018. Adis Nezirovic <anezirovic@haproxy.com>
 -- Copyright (c) 2017-2018. HAProxy Technologies, LLC.
--- All rights reserved
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, version 2 of the License
 
 local _author = "Adis Nezirovic <anezirovic@haproxy.com>"
 local _copyright = "Copyright 2017-2018. HAProxy Technologies, LLC. All Rights Reserved."
