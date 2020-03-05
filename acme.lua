d65db16924aa082ab211585ee7c6b35a98bc15f0
Adis Nezirovic <anezirovic@haproxy.com>
2020-03-05 12:27:51 +0100

    Change LICENSE to Apache v2.0
    Switching to Apache License to better align with other tools released
    by HAProxy Technologies and allow wider community involvement.

--- acme.lua
+++ acme.lua
@@ -6,15 +6,25 @@
 -- ACME RFC:
 -- https://tools.ietf.org/html/draft-ietf-acme-acme-12
 --
--- Copyright (c) 2017-2018. Adis Nezirovic <anezirovic@haproxy.com>
--- Copyright (c) 2017-2018. HAProxy Technologies, LLC.
+-- Copyright (c) 2017-2020. Adis Nezirovic <anezirovic@haproxy.com>
+-- Copyright (c) 2017-2020. HAProxy Technologies, LLC.
 --
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, version 2 of the License
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--    http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- SPDX-License-Identifier: Apache-2.0
 
 local _author = "Adis Nezirovic <anezirovic@haproxy.com>"
-local _copyright = "Copyright 2017-2018. HAProxy Technologies, LLC. All Rights Reserved."
+local _copyright = "Copyright 2017-2020. HAProxy Technologies, LLC."
 local _version = "1.0.0"
 
 local http = require "http"
